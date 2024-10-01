d2fd8157e60f947082635a78f9444c6bbe7fb5d6
AbishekBista <abishekbista84@gmail.com>
2024-10-01 18:20:35 +0545

    Added database_url and secret_key

--- Day 069/main.py
+++ Day 069/main.py
@@ -12,6 +12,7 @@ from sqlalchemy.orm import DeclarativeBase
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 from functools import wraps
+import os
 
 login_manager = LoginManager()
 
@@ -19,14 +20,14 @@ class Base(DeclarativeBase):
     pass
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(model_class=Base)
 
